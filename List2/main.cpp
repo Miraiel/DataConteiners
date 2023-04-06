77a25a40284d148f1e32b9e86868b5f7b606537a
pokidyuk <pokidyuk@MR012>
2023-04-06 14:51:51 +0300

    List_Erese DONE

--- List2/main.cpp
+++ List2/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 #define tab "\t"
+#define delimeter "\n------------------\n"
 
 class List
 {
@@ -25,17 +26,21 @@ class List
 
 	size_t size;
 public:
+	
 	List()
 	{
 		Head = Tail = nullptr;
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
+
 	~List()
 	{
+		while (Head)pop_front();
+		while (Tail)pop_back();
 		cout << "LDesturctor:\t" << this << endl;
 	}
-
+	
 	//------------------------Adding List---------------------------
 
 	void push_front(int Data)
@@ -43,18 +48,22 @@ public:
 		if (Head == nullptr && Tail == nullptr)
 		{
 			Head = Tail = new Element(Data);
-			size++;
-			return;
-		}
 
-		//создаем новый элемент
-		Element* New = new Element(Data);
-		//пристыковываем новый элемент к списку
-		New->pNext = Head;
-		//gпристыковываем список к новому элементу
-		Head->pPrev = New;
-		//пееводим голову на новый элеемнт
-		Head = New;
+		}
+		else
+		{
+
+			//создаем новый элемент
+			Element* New = new Element(Data);
+			//пристыковываем новый элемент к списку
+			New->pNext = Head;
+			//пристыковываем список к новому элементу
+			Head->pPrev = New;
+			//пееводим голову на новый элеемнт
+			Head = New;
+
+			//Head = new Element(Data, Head, );
+		}
 		size++;
 	}
 
@@ -65,6 +74,34 @@ public:
 		New->pPrev = Tail;
 		Tail->pNext = New;
 		Tail = New;
+
+		//Tail = new Element(Data, ,Tail);
+		size++;
+	}
+
+	void insert(int Data, int Index)
+	{
+		if (Index > size)return;
+		if (Index == 0)return push_front(Data);
+		Element* Temp;
+		if (Index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < Index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPrev;
+		}
+		//создаем новый элемент
+		Element* New = new Element(Data);
+		//прицепляем новый элемент к списку
+		New->pNext = Temp;
+		New->pPrev = Temp->pPrev;
+		//прицепляем список к новому элементу
+		Temp->pPrev->pNext = New;
+		Temp->pPrev = New;
 		size++;
 	}
 
@@ -94,22 +131,45 @@ public:
 		size--;
 	}
 
+	void erese(int Index)
+	{
+		if (Index > size)return;
+		if (Index == 0)return pop_front();
+		Element* Temp;
+		if (Index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < Index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPrev;
+		}
+
+		Temp->pPrev->pNext = Temp->pNext;
+		Temp->pNext->pPrev = Temp->pPrev;
+		delete Temp;
+		size--;
+	}
+
 	//--------------------------Methods-----------------------------
 
 	void print()const
 	{
 		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
 			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
-		cout << " :" << size << endl;
+		cout << "Количество элементов к списке :" << size << endl;
 	}
-	
-	void print()const
+
+	void reverse_print()const
 	{
 		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
 			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
-		cout << " :" << size << endl;
+		cout << "Количествол жлементов в списке :" << size << endl;
 	}
 };
+
 
 
 void main()
@@ -123,5 +183,24 @@ void main()
 	{
 		list.push_front(rand() % 100);
 	}
+	list.print();
+	cout << delimeter << endl;
+	list.reverse_print();
+
+
+	int value;
+	int index;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(value, index);
+	list.print();
+	cout << delimeter << endl;
+	list.reverse_print();
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	list.erese(index);
+	list.print();
+	cout << delimeter << endl;
+	list.reverse_print();
 
 }
